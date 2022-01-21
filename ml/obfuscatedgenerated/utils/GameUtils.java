df0c11e1f171ea5da1acf6ce795348ea3b1e8682
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-01-21 16:47:14 +0000

    create GameUtils with starter code (mainly greenfoot)

---
+++ ml/obfuscatedgenerated/utils/GameUtils.java
@@ -0,0 +1,23 @@
+package ml.obfuscatedgenerated.utils;
+
+/**
+ * Utils for game development.
+ * 
+ * @author @author obfuscatedgenerated
+ * @version 1.0.0
+ */
+
+public class GameUtils {
+    private static double initTime = System.currentTimeMillis();
+    private static double deltaTime = 0;
+
+    public static void update() { // IMPORTANT: call this every frame
+        double nowTime = System.currentTimeMillis();
+        deltaTime = (nowTime - initTime) / 1000;
+	    initTime = nowTime;
+    }
+
+    public static double getDelta() {
+        return deltaTime;
+    }
+}
---
+++ ml/obfuscatedgenerated/utils/greenfoot/GameUtils.java
@@ -0,0 +1,52 @@
+package ml.obfuscatedgenerated.utils.greenfoot;
+
+import greenfoot.*;
+
+import java.util.Map;
+import java.util.HashMap;
+
+/**
+ * Utils for game development.
+ * 
+ * @author @author obfuscatedgenerated
+ * @version 1.0.0
+ */
+
+public class GameUtils extends Actor {
+    private static double initTime = System.currentTimeMillis();
+    private static double deltaTime = 0;
+    private static Map<Actor,Double> exactX = new HashMap<>();
+    private static Map<Actor,Double> exactY = new HashMap<>();
+    
+    public static void setExactLocation(Actor object, double x, double y) {
+        exactX.put(object,x);
+        exactY.put(object,y);
+        object.setLocation((int) x, (int) y);
+    }
+    
+    public static double getExactX(Actor object) {
+        if (exactX.containsKey(object)) {
+            return exactX.get(object);
+        } else {
+            return object.getX();
+        }
+    }
+    
+    public static double getExactY(Actor object) {
+        if (exactY.containsKey(object)) {
+            return exactY.get(object);
+        } else {
+            return object.getY();
+        }
+    }
+
+    public static void update() {
+	    double nowTime = System.currentTimeMillis();
+        deltaTime = (nowTime - initTime) / 1000;
+	    initTime = nowTime;
+    }
+
+    public static double getDelta() {
+        return deltaTime;
+    }
+}
